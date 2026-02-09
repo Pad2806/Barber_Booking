aa46a122af6c587bc61ed9dfe89759a0a595492b
Pad2806 <tuiladuyne28@gmail.com>
2026-02-09 09:21:08 +0700

    fix ci workflow + web lint

--- apps/web/src/types/next-auth.d.ts
+++ apps/web/src/types/next-auth.d.ts
@@ -1,5 +1,5 @@
-import NextAuth, { DefaultSession } from 'next-auth';
-import { JWT } from 'next-auth/jwt';
+import type { DefaultSession } from 'next-auth';
+import type { JWT as DefaultJWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
   interface Session {
@@ -20,7 +20,7 @@ declare module 'next-auth' {
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT {
+  interface JWT extends DefaultJWT {
     id: string;
     role: string;
     accessToken: string;
