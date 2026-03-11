49d17ad8aef5897acfc4e3009fa795af3010a88e
Pad2806 <tuiladuyne28@gmail.com>
2026-03-11 22:14:30 +0700

    fix(api): provide StaffModule to AdminModule to fix DI error in AdminController

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { BookingsModule } from '../bookings/bookings.module';
+import { StaffModule } from '../staff/staff.module';
 import { AnalyticsService } from './analytics.service';
 
 @Module({
-  imports: [BookingsModule],
+  imports: [BookingsModule, StaffModule],
   controllers: [AdminController],
   providers: [AdminService, AnalyticsService],
   exports: [AdminService],
